03760bcdc47ac481e513ad149639246841cbaff6
admin <admin@admins-MacBook-Air.local>
2019-05-08 16:50:31 +0700

    first blood

---
+++ build/precache-manifest.0712db233f5b5f312b6bd40238333fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ba28180011f36cbd9050eb84d110ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d19c543524096318ad4",
+    "url": "/static/css/2.4358233e.chunk.css"
+  },
+  {
+    "revision": "e3920f49f6569257cafd",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "8d19c543524096318ad4",
+    "url": "/static/js/2.39c650ca.chunk.js"
+  },
+  {
+    "revision": "e3920f49f6569257cafd",
+    "url": "/static/js/main.cf5fb066.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
